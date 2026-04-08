559637a8623dea23a3eb4ac606a0436cef3892c7
yruyrui <saito.ryuto.ss8@naist.ac.jp>
2026-04-08 10:28:09 +0900

    add some lsp servers

--- home/editor/plugins/lsp.nix
+++ home/editor/plugins/lsp.nix
@@ -8,10 +8,16 @@
     lsp = {
       enable = true;
       servers = {
+        # Bash
+        bashls.enable = true;
         # lua
         lua_ls.enable = true;
         # C++
         clangd.enable = true;
+        # Nix
+        nil_ls.enable = true;
+        # Python
+        ruff.enable = true;
         # Zig
         zls.enable = true;
       };
